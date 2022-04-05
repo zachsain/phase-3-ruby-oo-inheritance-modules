1cb5ccac20c83702d744e657e9fc33bc1865c04b
Zachary Sain <zachsain@gmail.com>
2022-04-05 16:51:03 -0500

    commit tree 7c1849c56c4756b3253cde886a1423e0713d8885

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer
+    include Dance
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+    extend MetaDancing
+  end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+    include Dance
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+
+    extend MetaDancing
+end
